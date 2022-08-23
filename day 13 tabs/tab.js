884251972c4b6d77cf33fb957ce75149fd470dbd
sushanttwayana <sushanttwayana1@gmail.com>
2022-08-23 13:06:32 +0545

    30 Days Challenge

---
+++ day 13 tabs/tab.js
@@ -0,0 +1,30 @@
+const btns = document.querySelectorAll(".btn");
+const about =  document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function(e){
+    // console.log(e.target); // about ko sabai clicked child lai return garxa.
+
+    const id = e.target.dataset.id;
+
+    if(id) {
+        //remove active from other buttons
+
+        btns.forEach(function (btn) {
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+
+        //hide other articles
+        articles.forEach( function (article){
+            article.classList.remove("active");
+        
+        });
+
+       const element = document.getElementById(id);
+        element.classList.add("active");
+        // console.log(element);
+        
+    }
+
+});
