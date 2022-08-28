d6f7443a8c4a4f634201442086c0b36631a74667
sushanttwayana <sushanttwayana1@gmail.com>
2022-08-28 21:08:29 +0545

    30 Days Challenge

---
+++ day 17 dark-mode/d.js
@@ -0,0 +1,89 @@
+const articles = [
+    {
+        id : 1,
+        title : 'Be willing to see the unchangeable change.',
+        date: new Date(2022, 1, 30),
+        length:11,
+        snippet: `The first step in creating massive change in your life is not actually
+        believing that it's possible,  it's being willing to see if it is possible.
+        You are not going to be able to jump from being a complete skeptic to a wholehearted believer. 
+        The step between those is just being open to seeing what could be possible. `,
+    },
+
+    {
+        id : 2,
+        title : 'Give yourself permission to be successful.',
+        date: new Date(2021, 5, 24),
+        length:8,
+        snippet: `Instead of regurgitating the same old narrative of believing you'll be 
+        happy once you're 10 pounds,  one promotion and two life events down-the-line,
+         work on changing your inner monologue to: "I allow my life to be good. `,
+    },
+
+    {
+        id : 3,
+        title : "Don't allow other people's fears to cast shadows of doubt..",
+        date: new Date(2020, 9, 4),
+        length:5,
+        snippet: `The way people respond to news of your success will tell you how they are really doing in their lives.
+         If you announce your engagement, people who are in happy marriages will be 
+         elated for you. People who are in unhappy marriages will warn you that it is 
+         difficult and that you should enjoy your remaining time as "single" individuals.`,
+    },
+
+    {
+        id : 4,
+        title : 'Surround yourself with positive reinforcement.',
+        date: new Date(2018, 7, 11),
+        length:7,
+        snippet: `The first step in creating massive change in your life is not actually
+        believing that it's possible,  it's being willing to see if it is possible.
+        You are not going to be able to jump from being a complete skeptic to a wholehearted believer. 
+        The step between those is just being open to seeing what could be possible. `,
+    },
+
+    {
+        id : 5,
+        title : ' Speak your success as a present fact, not a future plan.',
+        date: new Date(2015, 12, 30),
+        length:5,
+        snippet: `Though you shouldn't say things like "I drive a convertible," or "I am a CEO," if they
+         are not in fact true, do start speaking about what it is you want out of life not 
+         in the context that you will one day pursue it, but that you are already living it.`,
+    },
+];
+
+const toggleBtn = document.querySelector(".btn");
+const articlesContainer = document.querySelector(".articles");
+
+toggleBtn.addEventListener("click", () => {
+    // select root element (html), toggle .dark-theme
+    document.documentElement.classList.toggle("dark-theme");
+});
+
+
+// iterate over, return article.post for each article
+const articlesData = articles.map((article) => {
+
+    // import moment.js and format date
+    const {title, date, length, snippet} = article;
+    const formatDate = moment(date).format('MMMM Do, YYYY');
+
+    return `<article class="post">
+            
+            <h2>${title}</h2>
+
+            <div class="post-info">
+                <span> ${formatDate}</span>
+                <span> ${length} min read</span>
+            </div>
+            
+            <p>
+             ${snippet}
+            </p>
+            
+            </article>`;
+
+}) .join();
+
+articlesContainer.innerHTML = articlesData;
