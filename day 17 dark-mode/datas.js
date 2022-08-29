014b387f6d9dbefb2b912249450242e33b4dac9d
sushanttwayana <sushanttwayana1@gmail.com>
2022-08-29 23:25:31 +0545

    30 Days Challenge

--- day 17 dark-mode/datas.js
+++ day 17 dark-mode/datas.js
@@ -7,7 +7,7 @@ const articles = [
         snippet: `The first step in creating massive change in your life is not actually
         believing that it's possible,  it's being willing to see if it is possible.
         You are not going to be able to jump from being a complete skeptic to a wholehearted believer. 
-        The step between those is just being open to seeing what could be possible. `,
+        The step between those is just being open to seeing what could be possible. `
     },
 
     {
@@ -17,7 +17,7 @@ const articles = [
         length:8,
         snippet: `Instead of regurgitating the same old narrative of believing you'll be 
         happy once you're 10 pounds,  one promotion and two life events down-the-line,
-         work on changing your inner monologue to: "I allow my life to be good. `,
+         work on changing your inner monologue to: "I allow my life to be good. `
     },
 
     {
@@ -28,7 +28,7 @@ const articles = [
         snippet: `The way people respond to news of your success will tell you how they are really doing in their lives.
          If you announce your engagement, people who are in happy marriages will be 
          elated for you. People who are in unhappy marriages will warn you that it is 
-         difficult and that you should enjoy your remaining time as "single" individuals.`,
+         difficult and that you should enjoy your remaining time as "single" individuals.`
     },
 
     {
@@ -39,7 +39,7 @@ const articles = [
         snippet: `The first step in creating massive change in your life is not actually
         believing that it's possible,  it's being willing to see if it is possible.
         You are not going to be able to jump from being a complete skeptic to a wholehearted believer. 
-        The step between those is just being open to seeing what could be possible. `,
+        The step between those is just being open to seeing what could be possible. `
     },
 
     {
@@ -49,6 +49,6 @@ const articles = [
         length:5,
         snippet: `Though you shouldn't say things like "I drive a convertible," or "I am a CEO," if they
          are not in fact true, do start speaking about what it is you want out of life not 
-         in the context that you will one day pursue it, but that you are already living it.`,
+         in the context that you will one day pursue it, but that you are already living it.`
     },
 ];
--- day 18 scroll/scroll.js
+++ day 18 scroll/scroll.js
@@ -1,1 +1,101 @@
- 
+ // Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+//set date
+const date = document.getElementById("date");
+date.innerHTML =  new Date().getFullYear();
+
+//close Links
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function(){
+    // linksContainer.classList.add("show-links");
+
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    // console.log(containerHeight); height = 0 by deafult 
+    const linksHeight = links.getBoundingClientRect().height;
+    // console.log(linksHeight);
+
+    if(containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`;
+    }
+
+    else {
+        linksContainer.style.height = 0;
+    }
+});
+
+//fix navbar
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener('scroll',function () {
+    // console.log(window.pageYOffset);
+
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if( scrollHeight > navHeight) {
+        navbar.classList.add("fixed-nav");
+    }
+
+    else {
+        navbar.classList.remove("fixed-nav");
+    }
+
+    if (scrollHeight > 500) {
+        console.log("helo");
+    
+        topLink.classList.add("show-link");
+      } 
+      else {
+        topLink.classList.remove("show-link");
+      }
+});
+
+// smooth scroll
+// select links
+
+const scrollLinks =  document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach( function (link) {
+
+    link.addEventListener("click", function(e) {
+        //prevent default
+        e.preventDefault();
+
+        //navigate to the specific spot
+        const id = e.currentTarget.getAttribute("href").slice(1); // slice le #about return nagri about matra garxa ie slice the #
+        const elements = document.getElementById(id);
+        // console.log(id);
+
+        // calculate the heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav");
+
+        let position =  elements.offsetTop - navHeight;
+        // console.log(position);
+
+        if (!fixedNav) {
+            position = position - navHeight;
+        }
+
+        // if(navHeight > 40) {
+        //     position = position + containerHeight;
+        // }
+
+        window.scrollTo ({
+            left : 0,
+            top : position,
+        });
+
+        linksContainer.style.height = 0;
+       
+    });
+});
+
